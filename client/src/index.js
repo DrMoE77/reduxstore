aa481837251dbe4b6e759530989e500b765e5ced
DrMoE77 <drmo.afifi@gmail.com>
2022-06-12 22:12:27 +1000

    enve

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import React from "react";
 import env from "react-dotenv";
 
 export function MyComponent() {
