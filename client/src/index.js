4ce6730a4e1d828ad4042225db2bf11e95a98450
DrMoE77 <drmo.afifi@gmail.com>
2022-06-12 21:18:18 +1000

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,14 @@
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
-
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_L1f0e3XAzjsG7jtp4uN7L9ql');
-
-function App() {
-  const options = {
-    // passing the client secret obtained from the server
-    clientSecret: '{{CLIENT_SECRET}}',
-  };
-
-  return (
-    <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm />
-    </Elements>
-  );
-};
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import React from "react";
+import env from "react-dotenv";
+
+export function MyComponent() {
+  return <div>{env.API_URL}</div>;
+}
 
 ReactDOM.render(
   <React.StrictMode>
