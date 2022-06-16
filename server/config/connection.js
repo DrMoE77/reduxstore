0406275af99d7a112a5021b3fc9b374df3288dd4
DrMoE77 <drmo.afifi@gmail.com>
2022-06-17 00:16:04 +1000

    dbpricelesschemist

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://DrMoE77:Yasseen2015@cluster0.qusi2.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://DrMoE77:Yasseen2015@cluster0.qusi2.mongodb.net/priclesschemist?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
